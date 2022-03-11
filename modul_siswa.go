7c88dba0b95e8ec39f298aeb7cca62dbd5e437fa
Prazt Andi <prasw4@gmail.com>
2022-03-11 21:53:04 +0700

    Update modul_siswa.go

--- modul_siswa.go
+++ modul_siswa.go
@@ -6,24 +6,24 @@ import (
 )
 
 type Student struct {
-	name  string
-	grade int
+	studentName string
+	grade       int
 }
 
 type Status func(string) bool
 
 func (s Student) Greeting() {
-	fmt.Printf("Hello %s from grade %d \n", s.name, s.grade)
+	fmt.Printf("Hello %s from grade %d \n", s.studentName, s.grade)
 }
 
 func (s Student) GetNameAt(i int) string {
-	return strings.Split(s.name, " ")[i-1]
+	return strings.Split(s.studentName, " ")[i-1]
 }
 
-func User(name string, status Status) {
-	if status(name) {
-		fmt.Printf("%s adalah siswa berstatus aktif \n", name)
+func User(userName string, status Status) {
+	if status(userName) {
+		fmt.Printf("%s adalah siswa berstatus aktif \n", userName)
 	} else {
-		fmt.Printf("%s adalah siswa berstatus tidak aktif \n", name)
+		fmt.Printf("%s adalah siswa berstatus tidak aktif \n", userName)
 	}
 }
